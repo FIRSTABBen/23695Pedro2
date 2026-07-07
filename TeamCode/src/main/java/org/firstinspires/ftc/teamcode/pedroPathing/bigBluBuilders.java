fa74f45c18f1865a2457b26a8a9c9c73b8fcbe37
ftc23695 <ftc23695@gmail.com>
2026-07-06 19:58:12 -0600

    poop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/bigBluBuilders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/bigBluBuilders.java
@@ -47,13 +47,14 @@ public class bigBluBuilders extends OpMode {
 
     Pathstate pathState;
 
-    private final Pose startPose = new Pose(123,122, Math.toRadians(36));
-    private final Pose movePose1 = new Pose(84, 84, Math.toRadians(36));
-    private final Pose movePose2 = new Pose(130,84, Math.toRadians(0));
-    private final Pose movePose3 = new Pose(84,84, Math.toRadians(0));
-//    private final Pose movePose7 = new Pose(144-56,8, Math.toRadians(90));
-//    private final Pose finalPose = new Pose(144-56, 20, Math.toRadians(90));
-
+    private final Pose startPose = new Pose(22,121, Math.toRadians(324));
+    private final Pose movePose1 = new Pose(59, 85, Math.toRadians(180));
+    private final Pose movePose2 = new Pose(24,85, Math.toRadians(180));
+    private final Pose movePose3 = new Pose(59,85, Math.toRadians(180));
+    private final Pose movePose4 = new Pose(59,58, Math.toRadians(180));
+    private final Pose movePose5 = new Pose(14, 52, Math.toRadians(180));
+    private final Pose movePose6 = new Pose(59,85, Math.toRadians(180));
+    private final Pose movePose7 = new Pose(59,130, Math.toRadians(180));
 
     //alice in pathChains\/
 
@@ -63,25 +64,24 @@ public class bigBluBuilders extends OpMode {
     private PathChain path4;
     private PathChain path5;
     private PathChain path6;
-//    private PathChain path7;
-//    private PathChain path8;
+    private PathChain path7;
+    private PathChain path8;
 //    private PathChain path9;
 //    private PathChain path10;
 //    private PathChain path11;
 
     private void shoot(){
         blocker.setPosition(0.6);
-        sleep(1000);
+        sleep(2000);
         intakeBack.setPower(1);
-        intakeForward.setPower(1);
-        sleep(1000);
+        sleep(2000);
         intakeBack.setPower(0);
-        intakeForward.setPower(0);
         blocker.setPosition(0.05);
     }
 
     public void statePathUpdate() {
-        shooter.setVelocity(2000);
+        shooter.setVelocity(1200);
+        intakeForward.setPower(1);
         target = false;
 
         //1100 for close
@@ -94,38 +94,30 @@ public class bigBluBuilders extends OpMode {
                 break;
             case PATH2:
                 if (!follower.isBusy()) {
-                    sleep(2000);
-                    shoot();
                     telemetry.addLine("done Path1");
                     follower.followPath(path2, true);
+                    shoot();
                     setPathState(Pathstate.PATH3);
                 }
                 break;
             case PATH3:
                 if (!follower.isBusy()) {
-                    intakeBack.setPower(1);
-                    intakeForward.setPower(1);
                     telemetry.addLine("done Path2");
                     follower.followPath(path3, true);
-                    sleep(250);
                     setPathState(Pathstate.PATH4);
                 }
                 break;
             //shoot
             case PATH4:
                 if (!follower.isBusy()) {
-                    intakeBack.setPower(0);
-                    intakeForward.setPower(0);
                     telemetry.addLine("done Path3");
                     follower.followPath(path4, true);
+                    shoot();
                     setPathState(Pathstate.PATH5);
                 }
                 break;
             case PATH5:
                 if (!follower.isBusy()) {
-
-                    sleep(2000);
-                    shoot();
                     telemetry.addLine("done Path4");
                     follower.followPath(path5, true);
                     setPathState(Pathstate.PATH6);
@@ -134,28 +126,27 @@ public class bigBluBuilders extends OpMode {
             case PATH6:
                 if (!follower.isBusy()) {
                     telemetry.addLine("done Path5");
-                    follower.followPath(path6, 0.5,true);
+                    follower.followPath(path6,true);
                     setPathState(Pathstate.PATH7);
                 }
                 break;
-//            case PATH7:
-//                if (!follower.isBusy()) {
-//                    telemetry.addLine("done Path6");
-//                    follower.followPath(path7, true);
-//                    setPathState(Pathstate.PATH8);
-//                }
-//                break;
-//            case PATH8:
-//                if (!follower.isBusy()) {
-//                    shoot();
-//                    telemetry.addLine("done Path7");
-//                    follower.followPath(path8, true);
-//                    setPathState(Pathstate.PATH9);
-//                }
-//                break;
+            case PATH7:
+                if (!follower.isBusy()) {
+                    telemetry.addLine("done Path6");
+                    follower.followPath(path7, true);
+                    shoot();
+                    setPathState(Pathstate.PATH8);
+                }
+
+                break;
+            case PATH8:
+                if (!follower.isBusy()) {
+                    telemetry.addLine("done Path7");
+                    terminateOpModeNow();
+                }
+                break;
             default:
                 telemetry.addLine("no state");
-                terminateOpModeNow();
                 break;
 
         }
@@ -180,22 +171,22 @@ public class bigBluBuilders extends OpMode {
                 .addPath(new BezierLine(movePose2, movePose3))
                 .setLinearHeadingInterpolation(movePose2.getHeading(), movePose3.getHeading())
                 .build();
-//        path7 = follower.pathBuilder()
-//                .addPath(new BezierLine(movePose5, startPose))
-//                .setLinearHeadingInterpolation(movePose5.getHeading(), startPose.getHeading())
-//                .build();
-//        path8 = follower.pathBuilder()
-//                .addPath(new BezierLine(startPose, movePose6))
-//                .setLinearHeadingInterpolation(startPose.getHeading(), movePose6.getHeading())
-//                .build();
-//        path9 = follower.pathBuilder()
-//                .addPath(new BezierLine(movePose6, movePose7))
-//                .setLinearHeadingInterpolation(movePose6.getHeading(), movePose7.getHeading())
-//                .build();
-//        path10 = follower.pathBuilder()
-//                .addPath(new BezierLine(movePose7, startPose))
-//                .setLinearHeadingInterpolation(movePose7.getHeading(), startPose.getHeading())
-//                .build();
+        path4 = follower.pathBuilder()
+                .addPath(new BezierLine(movePose3, movePose4))
+                .setLinearHeadingInterpolation(movePose3.getHeading(), movePose4.getHeading())
+                .build();
+        path5 = follower.pathBuilder()
+                .addPath(new BezierLine(movePose4, movePose5))
+                .setLinearHeadingInterpolation(movePose4.getHeading(), movePose5.getHeading())
+                .build();
+        path6 = follower.pathBuilder()
+                .addPath(new BezierLine(movePose5, movePose6))
+                .setLinearHeadingInterpolation(movePose5.getHeading(), movePose6.getHeading())
+                .build();
+        path7 = follower.pathBuilder()
+                .addPath(new BezierLine(movePose6, movePose7))
+                .setLinearHeadingInterpolation(movePose6.getHeading(), movePose7.getHeading())
+                .build();
 //        path11 = follower.pathBuilder()
 //                .addPath(new BezierLine(startPose, finalPose))
 //                .setLinearHeadingInterpolation(startPose.getHeading(), finalPose.getHeading())
